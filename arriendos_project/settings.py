901f946cd56c51e28282c603130e3cbadeb679b9
yonnbarlizaperalta-wq <yonnbarlizaperalta@gmail.com>
2026-06-02 22:45:47 -0500

    configuracion inicial del proyecto en settings.py

--- arriendos_project/settings.py
+++ arriendos_project/settings.py
@@ -1,3 +1,15 @@
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-clave-de-prueba'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -9,3 +21,54 @@ INSTALLED_APPS = [
     # tus apps
     'apps.clientes',
 ]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
+
+ROOT_URLCONF = 'arriendos.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],  # importante
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'arriendos.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+LANGUAGE_CODE = 'es'
+
+TIME_ZONE = 'America/Bogota'
+
+USE_I18N = True
+USE_TZ = True
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [  
+    BASE_DIR / 'static'
+]
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
