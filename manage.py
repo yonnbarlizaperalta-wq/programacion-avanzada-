2adf162e4069ca803b186a7a8167a53e45455261
yonnbarlizaperalta-wq <yonnbarlizaperalta@gmail.com>
2026-06-02 21:50:46 -0500

    pprimer commit

--- manage.py
+++ manage.py
@@ -1,1 +1,16 @@
-print ("hola mundo")
+#!/usr/bin/env python
+import os
+import sys
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arriendos.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+    main()
