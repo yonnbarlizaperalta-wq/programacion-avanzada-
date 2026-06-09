de02ad6249e7465a7c262f7553f8b352babd3636
yonnbarlizaperalta-wq <yonnbarlizaperalta@gmail.com>
2026-06-09 16:04:43 -0500

    creacion de carpeta urls en carpeta apps

--- arriendos/urls.py
+++ arriendos/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.clientes.urls')),  
+    path('', include('arriendos.urls')),  
 ]
