3e0c4d69c6d95a1f6de1742e2a0a697cda79f17d
yirjo <ospinogomezyairjose@gmail.com>
2026-06-10 22:27:27 -0500

    correcciones

--- apps/urls.py
+++ apps/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
+    path('admin/', admin.site.urls),
+    path('', include('apps.clientes.urls')),
 ]
