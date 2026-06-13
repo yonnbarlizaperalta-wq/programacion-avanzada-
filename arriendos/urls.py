867e63110c0e4259c5d48247f38c02674a76ce21
yonnbarlizaperalta-wq <yonnbarlizaperalta@gmail.com>
2026-06-12 20:46:33 -0500

    mis cambios antes de actualizar

--- arriendos/urls.py
+++ arriendos/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('arriendos.urls')),  
+    path('', include('apps.clientes.urls')),  # SOLO aquí se usa include
 ]
